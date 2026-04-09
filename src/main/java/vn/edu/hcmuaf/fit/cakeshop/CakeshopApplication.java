001ec2c23b685bf6d3889a1f8ed9218f901939a1
HQLuat <22130157@st.hcmuaf.edu.vn>
2026-04-09 22:30:02 +0700

    Initial commit

---
+++ src/main/java/vn/edu/hcmuaf/fit/cakeshop/CakeshopApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.cakeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeshopApplication.class, args);
+	}
+
+}
