3ba3080d2385bb7216d950a31b86041b53d1cfad
HQLuat <22130157@st.hcmuaf.edu.vn>
2026-05-30 18:13:44 +0700

    Fix login feature

--- src/main/java/vn/edu/hcmuaf/fit/cakeshop/modules/auth/dto/LoginResponseDTO.java
+++ src/main/java/vn/edu/hcmuaf/fit/cakeshop/modules/auth/dto/LoginResponseDTO.java
@@ -3,6 +3,7 @@ package vn.edu.hcmuaf.fit.cakeshop.modules.auth.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import vn.edu.hcmuaf.fit.cakeshop.modules.user.domain.entity.enums.UserRole;
 
 @Getter
 @Setter
@@ -10,4 +11,5 @@ import lombok.Setter;
 public class LoginResponseDTO {
     private String username;
     private String token;
+    private UserRole role;
 }
